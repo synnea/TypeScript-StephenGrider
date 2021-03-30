01a4e848806f56544edee33d2a2e5c96b027fbdb
Carina <carrie.poell@gmail.com>
2021-03-30 20:02:11 +0200

    add sorting algorithms

---
+++ sort/build/NumbersCollection.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NumbersCollection = void 0;
+var NumbersCollection = /** @class */ (function () {
+    function NumbersCollection(data) {
+        this.data = data;
+    }
+    Object.defineProperty(NumbersCollection.prototype, "length", {
+        // called a getter or accessor
+        get: function () {
+            return this.data.length;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    NumbersCollection.prototype.compare = function (leftIndex, rightIndex) {
+        return this.data[leftIndex] > this.data[rightIndex];
+    };
+    NumbersCollection.prototype.swap = function (leftIndex, rightIndex) {
+        var leftHand = this.data[leftIndex];
+        this.data[leftIndex] = this.data[rightIndex];
+        this.data[rightIndex] = leftHand;
+    };
+    return NumbersCollection;
+}());
+exports.NumbersCollection = NumbersCollection;
+// test section to demonstrate the getter
+// const collection = new NumbersCollection([1, 2, 3]);
+// collection.length;
---
+++ sort/src/NumbersCollection.ts
@@ -0,0 +1,21 @@
+export class NumbersCollection {
+  constructor(public data: number[]) {}
+
+  // called a getter or accessor
+  get length(): number {
+    return this.data.length;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftHand = this.data[leftIndex];
+    this.data[leftIndex] = this.data[rightIndex];
+    this.data[rightIndex] = leftHand;
+  }
+}
+
+// test section to demonstrate the getter
+// const collection = new NumbersCollection([1, 2, 3]);
+// collection.length;
