dcc0a6623928146cb0864c9e65e3eec785bef73c
Carina <carrie.poell@gmail.com>
2021-04-07 09:39:20 +0200

    create CsvFileReader class

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log('hi');
+Object.defineProperty(exports, "__esModule", { value: true });
+var CsvFileReader_1 = require("./CsvFileReader");
+var reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+// enum - enumeration
+// a set of closely related values
+var matchResult;
+(function (matchResult) {
+    matchResult["HomeWin"] = "H";
+    matchResult["AwayWin"] = "A";
+    matchResult["Draw"] = "D";
+})(matchResult || (matchResult = {}));
+var manUnitedWins = 0;
+for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+    var match = _a[_i];
+    if (match[1] === 'Man United' && match[5] === matchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === matchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log("Man United won " + manUnitedWins + " games");
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,24 @@
-console.log('hi');
+import { CsvFileReader } from './CsvFileReader';
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+// enum - enumeration
+// a set of closely related values
+enum matchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === matchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === matchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
